336b5d96178c09345b603ca8e09910e842f7e7e3
VinhHa3723 <vinh.ha3723@gmail.com>
2024-10-12 00:24:48 -0500

    CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -5,58 +5,53 @@
 
 public class CreditCard
 {
-	// The balance of the card
 	private Money balance;
-	
-	// The credit limit of the card
 	private Money creditLimit;
-	
-	// The owner of the card
 	private Person owner;
 	
 	/**
-      The CreditCard method
-      @param newCardHolder The owner of the card.
-	  @param limit The credit limit of the card.
-    */
+           The CreditCard method
+           @param newCardHolder The owner of the card.
+	   @param limit The credit limit of the card.
+        */
 	public CreditCard(Person newCardHolder, Money limit)
 	{
-		creditLimit = limit;	// Initializes the credit limit field
-		owner = newCardHolder;	// Initializes the owner field
-		balance = new Money(0); // Initializes the balance field
+		creditLimit = limit;	
+		owner = newCardHolder;	
+		balance = new Money(0); 
 	}
 	
 	/**
-      The getBalance method
-      @return The balance of the card
-    */
+           The getBalance method
+           @return The balance of the card
+        */
 	public Money getBalance()
 	{
 		return new Money(balance);
 	}
 	
 	/**
-      The getCreditLimit method
-      @return The credit limit of the card
-    */
+           The getCreditLimit method
+           @return The credit limit of the card
+        */
 	public Money getCreditLimit()
 	{
 		return new Money(creditLimit);
 	}
 	
 	/**
-      The getPersonals method
-      @return The personal information of the owner
-    */
+           The getPersonals method
+           @return The personal information of the owner
+        */
 	public String getPersonals()
 	{
 		return owner.toString();
 	}
 	
 	/**
-      The charge method
-      @param amount The amount of money spent
-    */
+           The charge method
+           @param amount The amount of money spent
+        */
 	public void charge(Money amount)
 	{
 		if (balance.add(amount).compareTo(creditLimit) < 0)
@@ -71,9 +66,9 @@ public class CreditCard
 	}
 	
 	/**
-      The payment method
-      @param amount The amount of money that was paid 
-    */
+           The payment method
+           @param amount The amount of money that was paid 
+        */
 	public void payment(Money amount)
 	{
 		System.out.println("Payment: " + amount);
