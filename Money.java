629900290af8857fedbfa239b10de10917f1a58a
VinhHa3723 <vinh.ha3723@gmail.com>
2024-10-15 23:05:28 -0500

    Money.java

--- Money.java
+++ Money.java
@@ -1,40 +1,52 @@
-public class Money {
+public class Money 
+{
     private double amount;
 
     // Constructor to initialize the amount
-    public Money(double amount) {
+    public Money(double amount) 
+    {
         this.amount = amount;
     }
 
     // Copy constructor
-    public Money(Money other) {
+    public Money(Money other) 
+    {
         this.amount = other.amount;
     }
 
     // Method to add two Money objects
-    public Money add(Money other) {
+    public Money add(Money other) 
+    {
         return new Money(this.amount + other.amount);
     }
 
     // Method to subtract two Money objects
-    public Money subtract(Money other) {
+    public Money subtract(Money other) 
+    {
         return new Money(this.amount - other.amount);
     }
 
     // Method to compare two Money objects
-    public int compareTo(Money other) {
-        if (this.amount < other.amount) {
+    public int compareTo(Money other) 
+    {
+        if (this.amount < other.amount) 
+        {
             return -1;
-        } else if (this.amount > other.amount) {
+        } 
+        else if (this.amount > other.amount) 
+        {
             return 1;
-        } else {
+        } 
+        else 
+        {
             return 0;
         }
     }
 
     // Overriding the toString method to display monetary values
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return String.format("$%.2f", this.amount);
     }
 }
